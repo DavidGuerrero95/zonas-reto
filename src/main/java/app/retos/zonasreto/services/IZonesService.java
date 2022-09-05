abb974703a1d9568431904208c9046c2f32d6a69
Santiago <davids.guerrero@udea.edu.co>
2022-09-05 18:07:34 -0500

    Cambios

--- src/main/java/app/retos/zonasreto/services/IZonesService.java
+++ src/main/java/app/retos/zonasreto/services/IZonesService.java
@@ -15,4 +15,6 @@ public interface IZonesService {
     Integer crearZonasPostes(Integer idEvents, List<Double> location);
 
     Integer crearZonasEvents(String idEvents, List<Double> location);
+
+    Integer modificarZonasEvents(String idEvents, List<Double> location, Integer zoneCode);
 }
